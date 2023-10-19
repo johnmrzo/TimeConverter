c783a773b076de7f10fab452ed039552592c3af3
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2023-10-19 11:34:50 -0400

    Time converter app started

---
+++ TimeConverter/TimeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverterApp.swift
+//  TimeConverter
+//
+//  Created by Bakhtiyorjon Mirzajonov on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
